6bcf092aaa721a37f064cf794f959c087beaacc1
Imron Gamidli <imron.gamidli@lt.hs-fulda.de>
2021-05-26 09:10:40 +0000

    comments

--- exercises/simple_throttle/controller1.py
+++ exercises/simple_throttle/controller1.py
@@ -3,12 +3,13 @@ from p4utils.utils.sswitch_API import SimpleSwitchAPI
 from scapy.all import Ether, sniff, Packet, BitField
 
 MIRROR_SESSION_ID = 99
+
 class CpuHeader(Packet):
     name = 'CpuPacket'
     fields_desc = [BitField('fid', 0, 32)]
 
 def msg_receive(pkt):
-    packet = Packet(str(pkt))
+    packet = Packet(str(pkt)) 
     cpu_header = CpuHeader(bytes(packet.payload))
     print(str(cpu_header.fid))
 
@@ -20,8 +21,6 @@ if __name__ == "__main__":
     thrift_port = topo.get_thrift_port(switchName)
     cpu_port = topo.get_cpu_port_index(switchName)
     controller = SimpleSwitchAPI(thrift_port)
-    # for x in range(10):
-    #     print(controller.register_read("MyIngress.dropRates", x))
 
    
     controller.mirroring_add(MIRROR_SESSION_ID, cpu_port)
